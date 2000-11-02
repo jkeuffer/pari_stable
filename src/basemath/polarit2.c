a91c59a50dde09b729f90d4c9020ce850240fd96
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2000-11-02 17:34:15 +0000

    make sure pgcd from polgcdnun is normalized

--- src/basemath/polarit2.c
+++ src/basemath/polarit2.c
@@ -2123,6 +2123,7 @@ polgcdnun(GEN x, GEN y)
     if (gcmp0(p1))
     {
       avma = av1;
+      if (lgef(y) == 3) { avma = av; return gun; }
       return (y==yorig)? gcopy(y): gerepileupto(av,y);
     }
     x = y; y = p1;
