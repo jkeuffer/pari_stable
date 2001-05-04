76442a6d631994e9a1064fc544d94b8d8967818d
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2001-05-04 10:32:42 +0000

    avoid "left shift count >= width of type" warning

--- config/endian.c
+++ config/endian.c
@@ -7,10 +7,10 @@ main()
     char c[sizeof(long)];
   } u;
 
-  if (sizeof(long) > 4)
-    u.l = (0x08070605L << 32) | 0x04030201L;
+  if (sizeof(long) > 4) /* avoid "left shift count >= width of type" warning */
+    u.l = (0x80706050UL << 28) | 0x04030201UL;
   else
-    u.l = 0x04030201L;
+    u.l = 0x04030201UL;
   for (i = 0; i < sizeof(long); i++)
     printf("%c", u.c[i]+'0');
   printf("\n");
