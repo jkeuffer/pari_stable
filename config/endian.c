6ebafe580e3eb12b4cacf45119f55b64e960fbf2
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2001-10-12 19:26:35 +0000

    "double endianness" is what matters

--- config/endian.c
+++ config/endian.c
@@ -1,19 +1,21 @@
 main()
 {
-  int i;
-  union
+  if (sizeof(long) == 4)
   {
-    unsigned long l;
-    char c[sizeof(long)];
-  } u;
-
-  if (sizeof(long) > 4) /* avoid "left shift count >= width of type" warning */
-    u.l = (0x80706050UL << 28) | 0x04030201UL;
+    union { double f; unsigned long i[2] } fi;
+    fi.f = 2.;
+    if (fi.i[0]==0 && fi.i[1]==(1UL<<30)) printf("1234\n");
+    else if (fi.i[1]==0 && fi.i[0]==(1UL<<30)) printf("4321\n");
+    else
+      printf("UNKNOWN\n");
+  }
   else
-    u.l = 0x04030201UL;
-  for (i = 0; i < sizeof(long); i++)
-    printf("%c", u.c[i]+'0');
-  printf("\n");
+  {
+    union { double f; unsigned long i } fi;
+    fi.f = 2.;
+    if (fi.i==(1UL<<62)) printf("12345678\n");
+    else
+      printf("UNKNOWN\n");
+  }
   exit(0);
 }
-
