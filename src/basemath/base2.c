345a207c70de709818b805e60576756061ab188e
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2002-09-03 20:10:32 +0000

    typo in random_unif_loop_pol

--- src/basemath/base2.c
+++ src/basemath/base2.c
@@ -1861,9 +1861,8 @@ random_unif_loop_pol(GEN nf, GEN P, GEN D, GEN Dp, GEN beta, GEN pol,
     if (DEBUGLEVEL && (++c & 0x3f) == 0) fprintferr("%d ", c);
     a = random_elt_in_P(beta, small);
     if (!a) continue;
-    if (small) a = small_to_col(a);
+    if (small) a = small_to_pol(a, varn(pol));
     a = centermod(a, Dp);
-    a = vec_to_pol(a, varn(pol));
     if ((a = prime_check_elt(D,Dp,a,pol,q,ramif)))
     {
       if (DEBUGLEVEL) fprintferr("\n");
