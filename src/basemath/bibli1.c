06643a1da64f989cdb89e278da18f7e09e4bb54f
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2002-11-21 18:21:38 +0000

    make Householder detection of accuracy loss independant of sizeof(long)

--- src/basemath/bibli1.c
+++ src/basemath/bibli1.c
@@ -134,7 +134,7 @@ FindApplyQ(GEN x, GEN mu, GEN B, long k, GEN Q, long prec)
   }
   else
     for (i=1; i<k; i++) coeff(mu,i,k) = x[i];
-  return (typ(x2) != t_REAL || lg(x2) >  3 || expo(x2) < 10);
+  return (typ(x2) != t_REAL || lg(x2) >  3 || expo(x2) < (BITS_IN_LONG>>1));
 }
 
 static void
