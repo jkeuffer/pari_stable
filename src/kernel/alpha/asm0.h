78e59dde86298c948b6e6a82d445346cbe0fe066
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2003-02-09 18:26:57 +0000

    remove extraneous 'register' declarations

--- src/kernel/alpha/asm0.h
+++ src/kernel/alpha/asm0.h
@@ -19,8 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 #define LOCAL_HIREMAINDER
 
 BEGINEXTERN
-  extern ulong overflow;
-  extern ulong hiremainder;
+  extern ulong overflow, hiremainder;
   extern long addll(ulong a, ulong b);
   extern long addllx(ulong a, ulong b);
   extern long subll(ulong a, ulong b);
@@ -37,7 +36,7 @@ ENDEXTERN
 #define LOCAL_OVERFLOW     register ulong overflow
 
 #define addll(a, b)\
-({ register ulong __value, __arg1 = (a), __arg2 = (b); \
+({ ulong __value, __arg1 = (a), __arg2 = (b); \
   __asm__ ("addq %2,%3,%0\n\tcmpult %4,%2,%1" \
    : "=&r" (__value), "=r" (overflow) \
    : "r" (__arg1), "r" (__arg2), "0" ((ulong) 0)); \
@@ -45,7 +44,7 @@ ENDEXTERN
 })
 
 #define addllx(a, b)\
-({ register ulong __value, __arg1 = (a), __arg2 = (b), __temp; \
+({ ulong __value, __arg1 = (a), __arg2 = (b), __temp; \
  __asm__ ("addq %3,%4,%0\n\tcmpult %5,%3,%2\n\taddq %5,%6,%0\n\tcmpult %5,%6,%1\n\taddq %6,%7,%1\n\t" \
    : "=&r" (__value), "=r" (overflow), "=r" (__temp) \
    : "r" (__arg1), "r" (__arg2), "0" ((ulong) 0), "1" (overflow), "2" ((ulong) 0)); \
@@ -53,7 +52,7 @@ __value; \
 })
 
 #define subll(a, b)\
-({ register ulong __value, __arg1 = (a), __arg2 = (b); \
+({ ulong __value, __arg1 = (a), __arg2 = (b); \
   __asm__ ("subq %2,%3,%0\n\tcmpult %2,%4,%1" \
    : "=&r" (__value), "=r" (overflow) \
    : "r" (__arg1), "r" (__arg2), "0" ((ulong)0)); \
@@ -61,7 +60,7 @@ __value; \
 })
 
 #define subllx(a, b)\
-({ register ulong __value, __arg1 = (a), __arg2 = (b), __temp1, __temp2; \
+({ ulong __value, __arg1 = (a), __arg2 = (b), __temp1, __temp2; \
 __asm__ ("subq %4,%5,%2\n\tcmpult %4,%8,%3\n\tsubq %8,%7,%0\n\tcmpult %8,%6,%1\n\taddq %7,%9,%1\n\t" \
    : "=r" (__value), "=r" (overflow), "=&r" (__temp1), "=r" (__temp2)  \
    : "r" (__arg1), "r" (__arg2), "0" ((ulong)0), "1" (overflow), "2" ((ulong)0), "3" ((ulong)0)); \
@@ -69,7 +68,7 @@ __asm__ ("subq %4,%5,%2\n\tcmpult %4,%8,%3\n\tsubq %8,%7,%0\n\tcmpult %8,%6,%1\n
 })
 
 #define shiftl(a, b) \
-({ register ulong __value, __arg1 = (a), __arg2 = (b), __temp; \
+({ ulong __value, __arg1 = (a), __arg2 = (b), __temp; \
  __asm__ ("subq %5,%4,%2\n\tsll %3,%4,%0\n\tsrl %3,%6,%1\n\t" \
    : "=r" (__value), "=r" (hiremainder), "=&r" (__temp) \
    : "r" (__arg1), "r" (__arg2), "n" ((ulong) 64), "2" ((ulong)0)); \
@@ -77,7 +76,7 @@ __asm__ ("subq %4,%5,%2\n\tcmpult %4,%8,%3\n\tsubq %8,%7,%0\n\tcmpult %8,%6,%1\n
 })
 
 #define shiftlr(a, b) \
-({ register ulong __value, __arg1 = (a), __arg2 = (b), __temp; \
+({ ulong __value, __arg1 = (a), __arg2 = (b), __temp; \
  __asm__ ("subq %5,%4,%2\n\tsrl %3,%4,%0\n\tsll %3,%6,%1\n\t" \
    : "=r" (__value), "=r" (hiremainder), "=&r" (__temp) \
    : "r" (__arg1), "r" (__arg2), "n" ((ulong) 64), "2" ((ulong)0)); \
@@ -85,7 +84,7 @@ __asm__ ("subq %4,%5,%2\n\tcmpult %4,%8,%3\n\tsubq %8,%7,%0\n\tcmpult %8,%6,%1\n
 })
 
 #define mulll(a, b) \
-({ register ulong __value, __arg1 = (a), __arg2 = (b); \
+({ ulong __value, __arg1 = (a), __arg2 = (b); \
  __asm__ ("umulh %2,%3,%1\n\tmulq %2,%3,%0\n\t" \
    : "=r" (__value), "=&r" (hiremainder) \
    : "r" (__arg1), "r" (__arg2)); \
@@ -93,7 +92,7 @@ __asm__ ("subq %4,%5,%2\n\tcmpult %4,%8,%3\n\tsubq %8,%7,%0\n\tcmpult %8,%6,%1\n
 })
 
 #define addmul(a, b) \
-({ register ulong __value, __arg1 = (a), __arg2 = (b), __temp; \
+({ ulong __value, __arg1 = (a), __arg2 = (b), __temp; \
  __asm__ ("mulq %3,%4,%0\n\tumulh %3,%4,%2\n\taddq %5,%6,%0\n\tcmpult %5,%6,%1\n\taddq %7,%6,%1\n\t" \
    : "=&r" (__value), "=r" (hiremainder), "=r" (__temp) \
    : "r" (__arg1), "r" (__arg2), "0" ((ulong) 0), "1" (hiremainder), "2" ((ulong) 0)); \
