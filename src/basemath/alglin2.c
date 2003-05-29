56aa83fc35bb6c2e0f1fbabce446ceebad665ee6
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2003-05-29 20:19:47 +0000

    typo in previous patch: 0 elementary divisors prepended twice

--- src/basemath/alglin2.c
+++ src/basemath/alglin2.c
@@ -3156,6 +3156,7 @@ THEEND:
   if (!U && !V)
   {
     if (typ(x) == t_MAT) x = mattodiagonal_i(x);
+    n = lg(x)-1;
     if (n0 != n) x = concatsp(zerovec(n0-n), x);
     return gerepilecopy(av0, x);
   }
