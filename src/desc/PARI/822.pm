d56e3b391ea639caa644b3283ee86ff4f7139b61
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2003-09-22 14:49:58 +0000

    Add copyright boilerplate.

--- src/desc/PARI/822.pm
+++ src/desc/PARI/822.pm
@@ -1,4 +1,19 @@
 #!/usr/bin/perl -w
+#Copyright (C) 2003  The PARI group.
+#
+#This file is part of the GP2C package.
+#
+#PARI/GP is free software; you can redistribute it and/or modify it under the
+#terms of the GNU General Public License as published by the Free Software
+#Foundation. It is distributed in the hope that it will be useful, but WITHOUT
+#ANY WARRANTY WHATSOEVER.
+#
+#Check the License for details. You should have received a copy of it, along
+#with the package; see the file 'COPYING'. If not, write to the Free Software
+#Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#Based on Debconf::Format::822 by Joey Hess <joey@kitenet.net>.
+
 package PARI::822;
 use strict;
 
