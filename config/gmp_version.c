af434d47608cbe1892e657107deb345532ac1ae0
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2003-10-17 00:14:17 +0000

    Make gmp_version.c C++ compatible.

--- config/gmp_version.c
+++ config/gmp_version.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
 #include <gmp.h>
-f() { mpn_gcdext(NULL,NULL, NULL, NULL, 0, NULL, 0); }
+void f(void) { mpn_gcdext(NULL,NULL, NULL, NULL, 0, NULL, 0); }
 main(){ printf("%s", gmp_version); }
