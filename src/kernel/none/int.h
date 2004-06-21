00fe8de24a86defb16e03c96cda3dacfff9c7784
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2004-06-21 13:36:28 +0000

    Add PARI_KERNEL_NONE macro

--- src/kernel/none/int.h
+++ src/kernel/none/int.h
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 /*x being a t_INT, return a pointer to the most significant word of x.*/
 
 #define int_LSW(x) ((x)+lgefint((x))-1)
-/*x being a t_int, return a pointer to the least significant word of x.*/
+/*x being a t_INT, return a pointer to the least significant word of x.*/
 
 #define int_precW(x) ((x)+1)
 /*x pointing to a mantissa word, return the previous (less significant)
@@ -31,3 +31,5 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 #define int_W(x,l) ((x)+lgefint((x))-1-(l))
 /*x being a t_INT, return a pointer to the l-th least significant word of x.*/
 
+#define PARI_KERNEL_NONE
+/*This macro should not be used in libpari itself.*/
