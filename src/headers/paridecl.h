e24ce846bca0d5d5005bc901ca1b2a9e29738d0c
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2004-09-04 10:49:53 +0000

    Make FpXQ_matrix_pow public.

--- src/headers/paridecl.h
+++ src/headers/paridecl.h
@@ -1540,6 +1540,7 @@ GEN     FpXQ_charpoly(GEN x, GEN T, GEN p);
 GEN     FpXQ_div(GEN x,GEN y,GEN T,GEN p);
 GEN     FpXQ_inv(GEN x,GEN T,GEN p);
 GEN     FpXQ_invsafe(GEN x, GEN T, GEN p);
+GEN     FpXQ_matrix_pow(long n, long m, GEN y, GEN P, GEN l);
 GEN     FpXQ_minpoly(GEN x, GEN T, GEN p);
 GEN     FpXQ_mul(GEN y,GEN x,GEN T,GEN p);
 GEN     FpXQ_pow(GEN x, GEN n, GEN T, GEN p);
