53810962bde43ba63ad43f14541fb2d3dc8a4f48
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2004-09-14 21:17:52 +0000

    improve readability

--- examples/matexp.c
+++ examples/matexp.c
@@ -1,13 +1,12 @@
 /* $Id$ */
-
 #include "pari.h"
 
 GEN
 matexp(GEN x,long prec)
 {
   pari_sp ltop = avma;
-  long lx=lg(x),i,k,n;
-  GEN y,r,s,p1,p2;
+  long lx = lg(x), i, k, n;
+  GEN xlow, y, r, s, p1, p2;
 
   /*@Ccom check that x is a square matrix */
   if (typ(x) != t_MAT) err(typeer,"matexp");
@@ -15,34 +14,32 @@ matexp(GEN x,long prec)
   if (lx != lg(x[1])) err(talker,"not a square matrix");
 
   /*@Ccom convert x to real or complex of real and compute its $L_2$ norm */
-  s = gzero; r = cgetr(prec+1); affsr(1,r); x = gmul(r,x);
-  for (i=1; i<lx; i++)
-    s = gadd(s, gnorml2((GEN)x[i]));
-  if (typ(s) == t_REAL) setlg(s,3);
-  s = gsqrt(s,3); /*@Ccom we do not need much precision on s */
+  x    = gmul(x, realun(prec+1)); /* @Ccom convert to floats */
+  xlow = gmul(x, realun(3));      /* @Ccom low precision copy */
+  s = gnorml2(xlow);
 
   /*@Ccom if s$ < 1$ we are happy */
-  k = expo(s);
+  k = expo(s); /*@Ccom s is guaranteed \typ{REAL} */
   if (k < 0) { n = 0; p1 = x; }
-  else { n = k+1; p1 = gmul2n(x,-n); setexpo(s,-1); }
+  else     { n = k+1; p1 = gmul2n(x, -n); s = gmul2n(s, -n); }
 
   /*@Ccom initializations before the loop */
-  y = gscalmat(r,lx-1); /*@Ccom creates scalar matrix with r on diagonal */
+  y = gscalmat(realun(prec+1), lx-1); /*@Ccom scalar identity matrix */
   p2 = p1; r = s; k = 1;
-  y = gadd(y,p2);
+  y = gadd(y, p2);
 
   /*@Ccom the main loop */
   while (expo(r) >= -BITS_IN_LONG*(prec-1))
   {
-    k++; p2 = gdivgs(gmul(p2,p1),k);
-    r = gdivgs(gmul(s,r),k); y = gadd(y,p2);
+    k++;
+    p2 = gdivgs(gmul(p2,p1), k);
+    r  = gdivgs(gmul(s,r),   k); y = gadd(y,p2);
   }
 
   /*@Ccom square back n times if necessary */
   for (i=0; i<n; i++) y = gsqr(y);
   return gerepileupto(ltop,y);
 }
-
 
 int
 main()
