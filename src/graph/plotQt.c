a5d18d7add758823f407f72009c14e9c20a82202
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2004-09-18 22:02:13 +0000

    undef grem which conflict with Qt headers.

--- src/graph/plotQt.c
+++ src/graph/plotQt.c
@@ -26,6 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 /////////////////////////////////////////////////////////////////////////////
 extern "C" {
 #include "pari.h"
+#undef grem
 #include "rect.h"
     void rectdraw0(long *, long *, long *, long, long);
 }
