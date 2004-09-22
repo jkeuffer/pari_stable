adc8edffa0568ebb6248d1ff0ba3d8fed2c889f7
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2004-09-22 17:28:35 +0000

    typo in gp2c type support: compatible --> !compatible

--- src/language/es.c
+++ src/language/es.c
@@ -126,7 +126,7 @@ filtre0(filtre_t *F)
         break;
 
       case ':':
-        if (compatible) {
+        if (!compatible) {
           t--; while (is_keyword_char(*s)) { s++; }
         }
         break;
