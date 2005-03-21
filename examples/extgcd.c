d9b2f5cf0aac9c2720ee13b5fccd487f07043eec
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2005-03-21 20:11:16 +0000

    gone -> gen_1

--- examples/extgcd.c
+++ examples/extgcd.c
@@ -5,7 +5,7 @@ GEN
 extgcd(GEN A, GEN B, GEN *U, GEN *V)
 {
   pari_sp av = avma;
-  GEN ux = gone, vx = gzero, a = A, b = B;
+  GEN ux = gen_1, vx = gen_0, a = A, b = B;
   if (typ(a) != t_INT || typ(b) != t_INT) err(typeer, "extgcd");
   if (signe(a) < 0) { a = negi(a); ux = negi(ux); }
   while (!gcmp0(b))
