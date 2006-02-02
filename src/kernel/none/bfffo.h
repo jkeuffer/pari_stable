73938f7284282b3b36c93ec2f99a3c98a2c43fb2
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2006-02-02 22:29:55 +0000

    add bfffo.h

---
+++ src/kernel/none/bfffo.h
@@ -0,0 +1,54 @@
+/* $Id$
+
+Copyright (C) 2000  The PARI group.
+
+This file is part of the PARI/GP package.
+
+PARI/GP is free software; you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation. It is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY WHATSOEVER.
+
+Check the License for details. You should have received a copy of it, along
+with the package; see the file 'COPYING'. If not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
+
+#if !defined(INLINE) || defined(INLINE_IS_STATIC)
+extern int  bfffo(ulong x);
+#else
+
+#if defined(__GNUC__) && !defined(DISABLE_INLINE)
+
+#define bfffo(x) \
+({\
+  static int __bfffo_tabshi[16]={4,3,2,2,1,1,1,1,0,0,0,0,0,0,0,0};\
+  int __value = BITS_IN_LONG - 4; \
+  ulong __arg1=(x); \
+#ifdef LONG_IS_64BIT
+  if (__arg1 & ~0xffffffffUL) {value -= 32; __arg1 >>= 32;}\
+#endif
+  if (__arg1 & ~0xffffUL) {__value -= 16; __arg1 >>= 16;} \
+  if (__arg1 & ~0x00ffUL) {__value -= 8; __arg1 >>= 8;} \
+  if (__arg1 & ~0x000fUL) {__value -= 4; __arg1 >>= 4;} \
+  __value + __bfffo_tabshi[__arg1]; \
+})
+
+#else
+
+INLINE int
+bfffo(ulong x)
+{
+  static int tabshi[16]={4,3,2,2,1,1,1,1,0,0,0,0,0,0,0,0};
+  int value = BITS_IN_LONG - 4;
+  ulong arg1=x;
+#ifdef LONG_IS_64BIT
+  if (arg1 & ~0xffffffffUL) {value -= 32; arg1 >>= 32;}
+#endif
+  if (arg1 & ~0xffffUL) {value -= 16; arg1 >>= 16;}
+  if (arg1 & ~0x00ffUL) {value -= 8; arg1 >>= 8;}
+  if (arg1 & ~0x000fUL) {value -= 4; arg1 >>= 4;}
+  return value + tabshi[arg1];
+}
+#endif
+
+#endif
