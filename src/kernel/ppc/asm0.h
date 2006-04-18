3248cddff8b262701c216d3abcea9ed7b23a1a1f
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2006-04-18 09:50:51 +0000

    typo divll.h --> divll

--- src/kernel/ppc/asm0.h
+++ src/kernel/ppc/asm0.h
@@ -14,7 +14,7 @@ with the package; see the file 'COPYING'. If not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 /*
 NOASM addll mulll bfffo
-ALWAYS divll.h
+ALWAYS divll
 */
 #ifdef ASMINLINE
 #define LOCAL_HIREMAINDER  register ulong hiremainder
