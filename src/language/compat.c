0bc831d94af51cba4d9d72f220c620d9ed884f39
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2006-10-01 12:18:02 +0000

    change error message "has been suppressed" -> "no longer exists"

--- src/language/compat.c
+++ src/language/compat.c
@@ -47,7 +47,7 @@ static GEN
 _factpol(GEN x, long t/*unused*/, long hint) {(void)t; return factpol(x,hint);}
 
 static void
-suppressed(void) {pari_err(talker,"this function has been suppressed");}
+suppressed(void) {pari_err(talker,"this function no longer exists");}
 
 #define BUCH_PROTO "GD0.3,G,D0.3,G,D5,G,D1,G,D4,L,D3,L,p"
 #define B_ARGS GEN g1,GEN g2,GEN g3,GEN g4,GEN g5,long l1,long l2,long prec
