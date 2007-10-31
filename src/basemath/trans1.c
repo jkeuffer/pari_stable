5098f9858a74fa08ea5db43d8076e7e5ddea034d
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2007-10-31 21:40:21 +0000

    typo in sqrt_ser [ sqrt(x) --> 2 should divide valuation (=2) ]

--- src/basemath/trans1.c
+++ src/basemath/trans1.c
@@ -927,7 +927,7 @@ sqrt_ser(GEN b, long prec)
   if (!signe(b)) return zeroser(vx, e>>1);
   a = shallowcopy(b);
   lx = lg(b); x = cgetg(lx, t_SER);
-  if (e & 1) pari_err(talker,"2 should divide valuation (= %ld) in sqrtn",2,e);
+  if (e & 1) pari_err(talker,"2 should divide valuation (= %ld) in sqrtn",e);
   a[1] = x[1] = evalsigne(1) | evalvarn(0) | _evalvalp(0);
   if (gissquareall(gel(a,2), &gel(x,2)) == gen_0)
     gel(x,2) = gsqrt(gel(a,2), prec);
