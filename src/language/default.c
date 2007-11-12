d9c6328c163f61ecbf06b72aa21e5ecf0ed7aa52
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2007-11-12 22:01:51 +0000

    useless inclusion of rect.h

--- src/language/default.c
+++ src/language/default.c
@@ -15,7 +15,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 #include "pari.h"
 #include "paripriv.h"
 #include "anal.h"
-#include "../graph/rect.h"
 
 #ifdef HAS_STRFTIME
 #  include <time.h>
