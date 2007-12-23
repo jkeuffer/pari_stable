316972aca3ee63ca7df8df252ffe9c5a512ce1ef
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2007-12-23 00:53:38 +0000

    pari_colormap undefined

--- src/graph/plotfltk.c
+++ src/graph/plotfltk.c
@@ -21,6 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 /////////////////////////////////////////////////////////////////////////////
 extern "C" {
 #include "pari.h"
+#include "paripriv.h"
 #include "rect.h"
 }
 
