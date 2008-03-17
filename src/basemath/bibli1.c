e34490d63a838009560c246a03dfbfc4f6664ce0
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2008-03-17 21:46:56 +0000

    Update to new interface

--- src/basemath/bibli1.c
+++ src/basemath/bibli1.c
@@ -617,12 +617,7 @@ lllint_i(GEN x, long D, long flag, GEN *ptB)
       *ptB = (n == 0)? cgetg(1, t_VEC): mkvec( gsqr(gcoeff(x,1,1)) );
     return lll_trivial(x,flag);
   }
-  if (flag==LLL_INPLACE)
-  {
-    x = LLLint(x,D,ptB);
-    if (ptB) *ptB = GS_norms(*ptB, DEFAULTPREC);
-    return x;
-  }
+  if (flag==LLL_INPLACE) return LLLint(x,D,ptB);
   fl = cgetg(lx,t_VECSMALL);
   hx = lg(x[1]);
   if (gram && hx != lx) pari_err(mattype1,"lllint");
