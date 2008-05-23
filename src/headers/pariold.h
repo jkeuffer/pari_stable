076310a5af32c58181738225e972b859fa613c18
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2008-05-23 22:42:47 +0000

    #define nfdiscf0(x) nfdisc0(x) #define discf(x) nfdisc(x)

--- src/headers/pariold.h
+++ src/headers/pariold.h
@@ -15,6 +15,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 
 /* For compatibility with 1.x.x */
 /*functions renamed*/
+#define nfdiscf0(x) nfdisc0(x)
+#define discf(x) nfdisc(x)
 #define discsr(x) poldisc0((x),-1)
 #define isfundamental Z_isfundamental
 #define factorpadic4 factorpadic
