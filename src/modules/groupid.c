2d9d383fbcedc285ff5f5247778c8eeecff6fa76
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2008-05-23 18:49:33 +0000

    group_ident: Fix error message.

--- src/modules/groupid.c
+++ src/modules/groupid.c
@@ -428,7 +428,7 @@ group_ident(GEN G, GEN S)
   pari_sp av = avma;
   long idx = group_ident_i(G, S);
   avma = av;
-  if (!idx) pari_err(impl,"galoisindex for groups of order >127");
+  if (!idx) pari_err(impl,"galoisidentify for groups of order >127");
   return idx;
 }
 
