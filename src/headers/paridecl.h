72759474ccf07b5da2a9818f4680e6fc38860523
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2008-09-23 15:39:58 +0000

    Fix alphabetic order of F2v_add_inplace

--- src/headers/paridecl.h
+++ src/headers/paridecl.h
@@ -44,8 +44,8 @@ GEN     isprincipalgenforce(GEN bnf,GEN x);
 /* F2x.c */
 
 GEN     F2c_to_ZC(GEN x);
-void    F2v_add_inplace(GEN x, GEN y);
 GEN     F2m_to_ZM(GEN z);
+void    F2v_add_inplace(GEN x, GEN y);
 GEN     F2x_1_add(GEN y);
 GEN     F2x_add(GEN x, GEN y);
 long    F2x_degree(GEN x);
