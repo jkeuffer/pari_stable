bcb1b4a4a6eeb60f79aae2d38137b19847bf4e94
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2009-01-28 00:21:19 +0000

    Fix asm constraint for addlx and subllx that could lead to broken code.

--- src/kernel/ix86/asm0.h
+++ src/kernel/ix86/asm0.h
@@ -42,7 +42,7 @@ ASM addll mulll bfffo divll
 #define addllx(a,b) \
 ({ ulong __value, __arg1 = (a), __arg2 = (b), __temp; \
    __asm__ ("subl %5,%2 ; adcl %4,%0 ; adcl %1,%1" \
-	: "=r" (__value), "=&r" (overflow), "=r" (__temp) \
+	: "=r" (__value), "=&r" (overflow), "=&r" (__temp) \
 	: "0" (__arg1), "g" (__arg2), "g" (overflow), "1" ((ulong)0), "2" ((ulong)0) \
 	: "cc"); \
   __value; \
@@ -61,7 +61,7 @@ ASM addll mulll bfffo divll
 #define subllx(a,b) \
 ({ ulong __value, __arg1 = (a), __arg2 = (b), __temp; \
    __asm__ ("subl %5,%2 ; sbbl %4,%0 ; adcl %1,%1" \
-	: "=r" (__value), "=&r" (overflow), "=r" (__temp) \
+	: "=r" (__value), "=&r" (overflow), "=&r" (__temp) \
 	: "0" (__arg1), "g" (__arg2), "g" (overflow), "1" ((ulong)0), "2" ((ulong)0) \
 	: "cc"); \
   __value; \
--- src/kernel/x86_64/asm0.h
+++ src/kernel/x86_64/asm0.h
@@ -35,7 +35,7 @@ ASM addll mulll bfffo divll
 #define addllx(a,b) \
 ({ ulong __value, __arg1 = (a), __arg2 = (b), __temp; \
    __asm__ ("subq %5,%2 ; adcq %4,%0 ; adcq %1,%1" \
-	: "=r" (__value), "=&r" (overflow), "=r" (__temp) \
+	: "=r" (__value), "=&r" (overflow), "=&r" (__temp) \
 	: "0" (__arg1), "g" (__arg2), "g" (overflow), "1" ((ulong)0), "2" ((ulong)0) \
 	: "cc"); \
   __value; \
@@ -53,7 +53,7 @@ ASM addll mulll bfffo divll
 #define subllx(a,b) \
 ({ ulong __value, __arg1 = (a), __arg2 = (b), __temp; \
    __asm__ ("subq %5,%2 ; sbbq %4,%0 ; adcq %1,%1" \
-	: "=r" (__value), "=&r" (overflow), "=r" (__temp) \
+	: "=r" (__value), "=&r" (overflow), "=&r" (__temp) \
 	: "0" (__arg1), "g" (__arg2), "g" (overflow), "1" ((ulong)0), "2" ((ulong)0) \
 	: "cc"); \
   __value; \
