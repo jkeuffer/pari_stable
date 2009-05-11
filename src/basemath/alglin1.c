90196e489861f5fe7b72a4a833bdfb10d2487cab
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2009-05-11 08:43:02 +0000

    Fix for 1/matrix(2,2,i,j,Mod(0, 2))

--- src/basemath/alglin1.c
+++ src/basemath/alglin1.c
@@ -1021,7 +1021,7 @@ FpM_gauss(GEN a, GEN b, GEN p)
     a = ZM_to_Flm(a, pp);
     b = ZM_to_Flm(b, pp);
     u = Flm_gauss_sp(a,b, pp);
-    if (!u) return NULL;
+    if (!u) {avma = av; return u;}
     u = iscol? Flc_to_ZC(gel(u,1)): Flm_to_ZM(u);
     return gerepileupto(av, u);
   }
