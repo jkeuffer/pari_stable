edc8cd4f57bdc0a3094168694a0c256aeb568a92
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2010-11-21 17:45:00 +0000

    remove 4th component of version() output (status).

--- src/language/init.c
+++ src/language/init.c
@@ -1832,18 +1832,30 @@ gettime(void) { return timer(); }
 /*******************************************************************/
 GEN
 pari_version(void) {
-  GEN v = cgetg(6, t_VEC);
-  const ulong mask = (1UL<<PARI_VERSION_SHIFT) - 1;
-  ulong major, minor, patch, n = PARI_VERSION_CODE;
-  patch = n & mask; n >>= PARI_VERSION_SHIFT;
-  minor = n & mask; n >>= PARI_VERSION_SHIFT;
-  major = n;
-  gel(v,1) = utoi(major);
-  gel(v,2) = utoi(minor);
-  gel(v,3) = utoi(patch);
-  gel(v,4) = strtoGENstr(PARI_STATUS);
-  gel(v,5) = strtoGENstr(PARI_RCSVERSION);
-  return v;
+  if (*PARI_RCSVERSION) {
+    GEN v = cgetg(5, t_VEC);
+    const ulong mask = (1UL<<PARI_VERSION_SHIFT) - 1;
+    ulong major, minor, patch, n = PARI_VERSION_CODE;
+    patch = n & mask; n >>= PARI_VERSION_SHIFT;
+    minor = n & mask; n >>= PARI_VERSION_SHIFT;
+    major = n;
+    gel(v,1) = utoi(major);
+    gel(v,2) = utoi(minor);
+    gel(v,3) = utoi(patch);
+    gel(v,4) = strtoGENstr(PARI_RCSVERSION);
+    return v;
+  } else {
+    GEN v = cgetg(4, t_VEC);
+    const ulong mask = (1UL<<PARI_VERSION_SHIFT) - 1;
+    ulong major, minor, patch, n = PARI_VERSION_CODE;
+    patch = n & mask; n >>= PARI_VERSION_SHIFT;
+    minor = n & mask; n >>= PARI_VERSION_SHIFT;
+    major = n;
+    gel(v,1) = utoi(major);
+    gel(v,2) = utoi(minor);
+    gel(v,3) = utoi(patch);
+    return v;
+  }
 }
 
 /* List of GP functions:
