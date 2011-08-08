60ca2106943ab99f89f4c7045568e4946e1256ac
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2011-08-08 20:31:54 +0000

    parice.h -> pariCE.h

--- src/headers/pari.h
+++ src/headers/pari.h
@@ -43,7 +43,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 #include <ctype.h>
 
 #ifdef WINCE
-#  include "parice.h"
+#  include "pariCE.h"
 #endif
 #include "parisys.h"
 #include "parigen.h"
