0dbe14d7ce55d018f401bb747789b6b674a40602
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2011-10-08 18:27:57 +0200

    fix member_err -> pari_err(e_TYPE,)

--- src/language/members.c
+++ src/language/members.c
@@ -35,22 +35,22 @@ INLINE int is_ell(GEN x) {
 }
 
 static void
-member_err(const char *s) { pari_err(e_TYPE,s); }
+member_err(const char *s, GEN y) { pari_err(e_TYPE,s,y); }
 
 GEN
 member_e(GEN x)
 {
-  x = get_prid(x);
-  if (!x) member_err("e");
-  return gel(x,3);
+  GEN y = get_prid(x);
+  if (!y) member_err("e",x);
+  return gel(y,3);
 }
 
 GEN
 member_f(GEN x)
 {
-  x = get_prid(x);
-  if (!x) member_err("f");
-  return gel(x,4);
+  GEN y = get_prid(x);
+  if (!y) member_err("f",x);
+  return gel(y,4);
 }
 
 GEN
@@ -60,14 +60,16 @@ member_p(GEN x)
   if (t == typ_GAL) return gal_get_p(x);
   switch(typ(x)) {
     case t_VEC:
-      x = get_prid(x); if (!x) member_err("p");
-      return pr_get_p(x);
+    {
+      GEN y = get_prid(x); if (!y) break;
+      return pr_get_p(y);
+    }
     case t_PADIC:
       return gel(x,2);
     case t_FFELT:
       return FF_p_i(x);
   }
-  member_err("p");
+  member_err("p",x);
   return NULL;
 }
 
@@ -79,15 +81,15 @@ member_bid(GEN x)
     case typ_BNR: return bnr_get_bid(x);
     case typ_BID: return x;
   }
-  member_err("bid");
+  member_err("bid",x);
   return NULL;
 }
 
 GEN
 member_bnf(GEN x)
 {
-  long t; x = get_bnf(x,&t);
-  if (!x) member_err("bnf");
+  long t; GEN y = get_bnf(x,&t);
+  if (!y) member_err("bnf",x);
   return x;
 }
 
@@ -97,7 +99,7 @@ member_nf(GEN x)
   long t; GEN y = get_nf(x,&t);
   if (!y) {
     if (t == typ_RNF) return gel(x,10);
-    member_err("nf");
+    member_err("nf",x);
   }
   return y;
 }
@@ -118,7 +120,7 @@ member_zk(GEN x)
       case typ_RNF:
         return gel(x,7);
     }
-    member_err("zk");
+    member_err("zk",x);
   }
   return nf_get_zk(y);
 }
@@ -134,7 +136,7 @@ member_disc(GEN x) /* discriminant */
       case typ_Q  : return quad_disc(x);
       case typ_ELL: return ell_get_disc(x);
     }
-    member_err("disc");
+    member_err("disc",x);
   }
   return nf_get_disc(y);
 }
@@ -154,7 +156,7 @@ member_pol(GEN x) /* polynomial */
     }
     if (typ(x)==t_POLMOD) return gel(x,2);
     if (typ(x)==t_FFELT) return FF_to_FpXQ(x);
-    member_err("pol");
+    member_err("pol",x);
   }
   return nf_get_pol(y);
 }
@@ -173,7 +175,7 @@ member_mod(GEN x) /* modulus */
     case t_INTMOD: case t_POLMOD: case t_QUAD: break;
     case t_PADIC: return gel(x,3);
     case t_FFELT: return FF_mod(x);
-    default: member_err("mod");
+    default: member_err("mod",x);
   }
   return gel(x,1);
 }
@@ -182,7 +184,7 @@ GEN
 member_sign(GEN x) /* signature */
 {
   long t; GEN y = get_nf(x,&t);
-  if (!y) member_err("sign");
+  if (!y) member_err("sign",x);
   return gel(y,2);
 }
 GEN
@@ -194,7 +196,7 @@ GEN
 member_index(GEN x)
 {
   long t; GEN y = get_nf(x,&t);
-  if (!y) member_err("index");
+  if (!y) member_err("index",x);
   return nf_get_index(y);
 }
 
@@ -212,24 +214,24 @@ nfmats(GEN x)
 GEN
 member_t2(GEN x) /* T2 matrix */
 {
-  long t; x = nfmats(get_nf(x,&t));
-  if (!x) member_err("t2");
-  return gram_matrix(gel(x,2));
+  long t; GEN y = nfmats(get_nf(x,&t));
+  if (!y) member_err("t2",x);
+  return gram_matrix(gel(y,2));
 }
 
 GEN
 member_diff(GEN x) /* different */
 {
-  long t; x = nfmats(get_nf(x,&t));
-  if (!x) member_err("diff");
-  return gel(x,5);
+  long t; GEN y = nfmats(get_nf(x,&t));
+  if (!y) member_err("diff",x);
+  return gel(y,5);
 }
 
 GEN
 member_codiff(GEN x) /* codifferent */
 {
   long t; GEN T, D, DinvT, nf = get_nf(x,&t), y = nfmats(nf);
-  if (!y) member_err("codiff");
+  if (!y) member_err("codiff",x);
   T = gel(y,4);
   D = absi(nf_get_disc(nf));
   DinvT = ZM_inv(T,D);
@@ -244,7 +246,7 @@ member_roots(GEN x) /* roots */
   {
     if (t == typ_ELL && is_ell(x)) return ell_get_roots(x);
     if (t == typ_GAL) return gal_get_roots(x);
-    member_err("roots");
+    member_err("roots",x);
   }
   return nf_get_roots(y);
 }
@@ -254,7 +256,7 @@ static GEN
 check_RES(GEN x, const char *s)
 {
   GEN y = gel(x,8);
-  if (typ(y) != t_VEC || lg(y) < 4) member_err(s);
+  if (typ(y) != t_VEC || lg(y) < 4) member_err(s,x);
   return y;
 }
 
@@ -275,7 +277,7 @@ member_clgp(GEN x) /* class group (3-component row vector) */
         case 3: /* no gen */
         case 4: return x;
       }
-    member_err("clgp");
+    member_err("clgp",x);
   }
   if (t==typ_BNR) return gel(x,5);
   y = check_RES(y, "clgp");
@@ -289,7 +291,7 @@ member_reg(GEN x) /* regulator */
   if (!y)
   {
     if (t == typ_QUA) return gel(x,4);
-    member_err("reg");
+    member_err("reg",x);
   }
   if (t == typ_BNR) pari_err(e_IMPL,"ray regulator");
   y = check_RES(y, "reg");
@@ -308,7 +310,7 @@ member_fu(GEN x) /* fundamental units */
         x = quad_disc(x);
         return (signe(x)<0)? cgetg(1,t_VEC): quadunit(x);
     }
-    member_err("fu");
+    member_err("fu",x);
   }
   if (t == typ_BNR) pari_err(e_IMPL,"ray units");
   return matbasistoalg(y, bnf_get_fu(y));
@@ -323,7 +325,7 @@ member_tu(GEN x)
   if (!bnf)
   {
     GEN y;
-    if (t != typ_Q) member_err("tu");
+    if (t != typ_Q) member_err("tu",x);
     y = quad_disc(x);
     if (signe(y) > 0 || cmpiu(y,4) > 0) return mkvec2(gen_m1, gen_2);
 
@@ -365,25 +367,25 @@ member_zkst(GEN x)
       if (typ(bid) == t_VEC && lg(bid) > 2) return gel(bid,2);
     }
   }
-  member_err("zkst");
+  member_err("zkst",x);
   return NULL; /* not reached */
 }
 
 GEN
-member_no(GEN clg) /* number of elements of a group (of type clgp) */
+member_no(GEN x) /* number of elements of a group (of type clgp) */
 {
   pari_sp av = avma;
-  clg = member_clgp(clg);
-  if (typ(clg)!=t_VEC || (lg(clg)!=3 && lg(clg)!=4)) member_err("no");
+  GEN clg = member_clgp(x);
+  if (typ(clg)!=t_VEC || (lg(clg)!=3 && lg(clg)!=4)) member_err("no",x);
   avma = av; return gel(clg,1);
 }
 
 GEN
-member_cyc(GEN clg) /* cyclic decomposition (SNF) of a group (of type clgp) */
+member_cyc(GEN x) /* cyclic decomposition (SNF) of a group (of type clgp) */
 {
   pari_sp av = avma;
-  clg = member_clgp(clg);
-  if (typ(clg)!=t_VEC || (lg(clg)!=3 && lg(clg)!=4)) member_err("cyc");
+  GEN clg = member_clgp(x);
+  if (typ(clg)!=t_VEC || (lg(clg)!=3 && lg(clg)!=4)) member_err("cyc",x);
   avma = av; return gel(clg,2);
 }
 
@@ -400,7 +402,7 @@ member_gen(GEN x)
   if (t == typ_GAL) return gal_get_gen(x);
   av = avma;
   x = member_clgp(x);
-  if (typ(x)!=t_VEC || lg(x)!=4) member_err("gen");
+  if (typ(x)!=t_VEC || lg(x)!=4) member_err("gen",x);
   avma = av;
   if (typ(x[1]) == t_COL) return gel(x,2); /* from bnfisprincipal */
   return gel(x,3);
@@ -410,7 +412,7 @@ member_group(GEN x)
 {
   long t; (void)get_nf(x,&t);
   if (t == typ_GAL) return gal_get_group(x);
-  member_err("group");
+  member_err("group",x);
   return NULL; /* not reached */
 }
 GEN
@@ -418,98 +420,98 @@ member_orders(GEN x)
 {
   long t; (void)get_nf(x,&t);
   if (t == typ_GAL) return gal_get_orders(x);
-  member_err("orders");
+  member_err("orders",x);
   return NULL; /* not reached */
 }
 
 GEN
 member_a1(GEN x)
 {
-  if (!is_ell5(x)) member_err("a1");
+  if (!is_ell5(x)) member_err("a1",x);
   return ell_get_a1(x);
 }
 
 GEN
 member_a2(GEN x)
 {
-  if (!is_ell5(x)) member_err("a2");
+  if (!is_ell5(x)) member_err("a2",x);
   return ell_get_a2(x);
 }
 
 GEN
 member_a3(GEN x)
 {
-  if (!is_ell5(x)) member_err("a3");
+  if (!is_ell5(x)) member_err("a3",x);
   return ell_get_a3(x);
 }
 
 GEN
 member_a4(GEN x)
 {
-  if (!is_ell5(x)) member_err("a4");
+  if (!is_ell5(x)) member_err("a4",x);
   return ell_get_a4(x);
 }
 
 GEN
 member_a6(GEN x)
 {
-  if (!is_ell5(x)) member_err("a6");
+  if (!is_ell5(x)) member_err("a6",x);
   return ell_get_a6(x);
 }
 
 GEN
 member_b2(GEN x)
 {
-  if (!is_smallell(x)) member_err("b2");
+  if (!is_smallell(x)) member_err("b2",x);
   return ell_get_b2(x);
 }
 
 GEN
 member_b4(GEN x)
 {
-  if (!is_smallell(x)) member_err("b4");
+  if (!is_smallell(x)) member_err("b4",x);
   return ell_get_b4(x);
 }
 
 GEN
 member_b6(GEN x)
 {
-  if (!is_smallell(x)) member_err("b6");
+  if (!is_smallell(x)) member_err("b6",x);
   return ell_get_b6(x);
 }
 
 GEN
 member_b8(GEN x)
 {
-  if (!is_smallell(x)) member_err("b8");
+  if (!is_smallell(x)) member_err("b8",x);
   return ell_get_b8(x);
 }
 
 GEN
 member_c4(GEN x)
 {
-  if (!is_smallell(x)) member_err("c4");
+  if (!is_smallell(x)) member_err("c4",x);
   return ell_get_c4(x);
 }
 
 GEN
 member_c6(GEN x)
 {
-  if (!is_smallell(x)) member_err("c6");
+  if (!is_smallell(x)) member_err("c6",x);
   return ell_get_c6(x);
 }
 
 GEN
 member_j(GEN x)
 {
-  if (!is_smallell(x)) member_err("j");
+  if (!is_smallell(x)) member_err("j",x);
   return ell_get_j(x);
 }
 
 GEN
 member_omega(GEN x)
 {
-  if (!is_ell(x)) member_err("omega");
+  if (!is_ell(x)) member_err("omega",x);
   if (!ell_is_real(x)) pari_err(e_MISC,"curve not defined over R");
   return mkvec2(gel(x,15), gel(x,16));
 }
@@ -517,7 +519,7 @@ member_omega(GEN x)
 GEN
 member_eta(GEN x)
 {
-  if (!is_ell(x)) member_err("eta");
+  if (!is_ell(x)) member_err("eta",x);
   if (!ell_is_real(x)) pari_err(e_MISC,"curve not defined over R");
   return mkvec2(gel(x,17), gel(x,18));
 }
@@ -525,7 +527,7 @@ member_eta(GEN x)
 GEN
 member_area(GEN x)
 {
-  if (!is_ell(x)) member_err("area");
+  if (!is_ell(x)) member_err("area",x);
   if (!ell_is_real(x)) pari_err(e_MISC,"curve not defined over R");
   return gel(x,19);
 }
@@ -533,7 +535,7 @@ member_area(GEN x)
 GEN
 member_tate(GEN x)
 {
-  if (!is_ell(x)) member_err("tate");
+  if (!is_ell(x)) member_err("tate",x);
   if (!ell_is_padic(x))
     pari_err(e_MISC,"curve not defined over a p-adic field");
   return mkvec3(gel(x,15), gel(x,16), gel(x,17));
@@ -542,7 +544,7 @@ member_tate(GEN x)
 GEN
 member_w(GEN x)
 {
-  if (!is_ell(x)) member_err("w");
+  if (!is_ell(x)) member_err("w",x);
   if (!ell_is_padic(x))
     pari_err(e_MISC,"curve not defined over a p-adic field");
   return gel(x,18);
