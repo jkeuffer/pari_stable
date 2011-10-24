jkeuffer/pari_stable

commit 7b985ff8a726e89191d2733b9052b2e2c228bfde

minor improvements root_error()

src/basemath/rootpol.c
@@ -1627,20 +1627,23 @@ root_error(long n, long k, GEN roots_pol, long pari_err, GEN shatzle)
   if (expo(rho) < 0) rho = real_1(DEFAULTPREC);
   eps = mulrr(rho, shatzle);
   aux = shiftr(powru(rho,n), pari_err);
+  prod = NULL; /* 1. */
 
-  for (j=1; j<=2 || (j<=5 && gcmp(rap, dbltor(1.2)) > 0); j++)
+  for (j=1; j<=2 || (j<=5 && cmprr(rap, dbltor(1.2)) > 0); j++)
   {
-    m = n; prod = real_1(DEFAULTPREC);
+    m = n;
     epsbis = mulrr(eps, dbltor(1.25));
     for (i=1; i<=n; i++)
     {
       if (i != k && cmprr(gel(d,i),epsbis) > 0)
       {
+        GEN dif = subrr(gel(d,i),eps);
+	prod = prod? mulrr(prod, dif): dif;
 	m--;
-	prod = mulrr(prod, subrr(gel(d,i),eps));
       }
     }
-    eps2 = sqrtnr(mpdiv(shiftr(aux,2*m-2), prod), m);
+    eps2 = prod? divrr(aux, prod): aux;
+    if (m > 1) eps2 = sqrtnr(shiftr(eps2, 2*m-2), m);
     rap = divrr(eps,eps2); eps = eps2;
   }
   return eps;

commit 2a258c781cf273a8fcbd6d8510319441c70d2cbe

fix typo introduced in 7b985ff8a726e89191d2733b9052b2e2c228bfde "minor improvements root_error()" prod was not re-initialized to 1.

src/basemath/rootpol.c
@@ -1614,8 +1614,8 @@ split_0(GEN p, long bit, GEN *F, GEN *G)
 static GEN
 root_error(long n, long k, GEN roots_pol, long err, GEN shatzle)
 {
-  GEN rho, d, eps, epsbis, eps2, prod, aux, rap = NULL;
-  long i, j, m;
+  GEN rho, d, eps, epsbis, eps2, aux, rap = NULL;
+  long i, j;
 
   d = cgetg(n+1,t_VEC);
   for (i=1; i<=n; i++)
@@ -1630,11 +1630,11 @@ root_error(long n, long k, GEN roots_pol, long err, GEN shatzle)
   if (expo(rho) < 0) rho = real_1(DEFAULTPREC);
   eps = mulrr(rho, shatzle);
   aux = shiftr(powru(rho,n), err);
-  prod = NULL; /* 1. */
 
   for (j=1; j<=2 || (j<=5 && cmprr(rap, dbltor(1.2)) > 0); j++)
   {
-    m = n;
+    GEN prod = NULL; /* 1. */
+    long m = n;
     epsbis = mulrr(eps, dbltor(1.25));
     for (i=1; i<=n; i++)
     {