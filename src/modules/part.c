f03668c6cbbe6f107cd0241c3ddf2e58e4a977b4
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2011-11-02 14:06:25 +0100

    e_MISC -> e_OVERFLOW

--- src/modules/part.c
+++ src/modules/part.c
@@ -160,7 +160,7 @@ numbpart(GEN n)
   if (signe(n) < 0) return gen_0;
   if (cmpiu(n, 2) < 0) return gen_1;
   if (cmpii(n, uu32toi(0x38d7e, 0xa4c68000)) >= 0)
-    pari_err(e_MISC, "arg to partition function must be < 10^15");
+    pari_err_OVERFLOW("numbpart [n < 10^15]");
   est = estim(n);
   bitprec = (long)(rtodbl(est)/LOG2) + 32;
   prec = nbits2prec(bitprec);
