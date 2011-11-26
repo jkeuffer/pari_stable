5a2697e7fde83053c2aa98a0b477991fca0c80e4
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2011-11-26 16:00:54 +0100

    mateigen used taker2/e_SYNTAX.

--- src/basemath/alglin1.c
+++ src/basemath/alglin1.c
@@ -3224,7 +3224,7 @@ eigen(GEN x, long prec)
     r3 = grndtoi(r2, &e); if (e < ex) r2 = r3;
     ssesp = ker_aux(RgM_Rg_add_shallow(x, gneg(r2))); l = lg(ssesp);
     if (l == 1 || ly + (l-1) > n)
-      pari_err(e_SYNTAX, "missing eigenspace. Compute the matrix to higher accuracy, then restart eigen at the current precision",NULL,NULL);
+      pari_err_PREC("mateigen");
     for (i=1; i<l; i++,ly++) gel(y,ly) = gel(ssesp,i); /* eigenspace done */
 
     r1=r2; /* try to find a different eigenvalue */
