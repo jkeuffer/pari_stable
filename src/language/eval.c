e5c4cbedd7603d726b404545dd4eb10e67a825ec
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2012-03-19 19:05:11 +0100

    eval.c: Fix comment about allocatemem
    allocatemem is now handled by longjmp.

--- src/language/eval.c
+++ src/language/eval.c
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 
 /********************************************************************/
 /*                                                                  */
-/*         break/next/return/allocatemem handling                   */
+/*                   break/next/return handling                     */
 /*                                                                  */
 /********************************************************************/
 
