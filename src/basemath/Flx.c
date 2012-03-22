838cc1d7c0a096d1a94bd163442516e5fb0d46bd
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2012-03-22 21:28:56 +0100

    Flxq_log_find_rel: rename z to rel.

--- src/basemath/Flx.c
+++ src/basemath/Flx.c
@@ -2163,9 +2163,9 @@ Flxq_log_find_rel(GEN b, long r, GEN T, ulong p, GEN *g, long *e)
       {
         GEN F = factorel(z, p);
         GEN G = factorel(gcoeff(M,1,1), p);
-        GEN z = mkmat2(vecsmall_concat(gel(F, 1),gel(G, 1)),
-                       vecsmall_concat(gel(F, 2),zv_neg(gel(G, 2))));
-        gerepileall(av,2,g,&z); return z;
+        GEN rel = mkmat2(vecsmall_concat(gel(F, 1),gel(G, 1)),
+                         vecsmall_concat(gel(F, 2),zv_neg(gel(G, 2))));
+        gerepileall(av,2,g,&rel); return rel;
       }
     }
     if (low_stack(lim, stack_lim(av,2)))
