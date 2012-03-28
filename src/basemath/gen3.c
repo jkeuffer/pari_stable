jkeuffer/pari_stable

commit 961d38a24ef49919b4dfa5b486dc9d168a12e7e0

25- wrong sign in polresultant() [#1234] + improve substvec memory use [ resultant bench ]

src/basemath/gen3.c
@@ -1465,24 +1465,29 @@ GEN
 gsubstvec(GEN e, GEN v, GEN r)
 {
   pari_sp ltop=avma;
-  long i,l=lg(v);
+  long i, j, l = lg(v);
   GEN w,z;
-  if ( !is_vec_t(typ(v)) || !is_vec_t(typ(r)) )
-    pari_err(typeer,"substvec");
+  if ( !is_vec_t(typ(v)) || !is_vec_t(typ(r)) ) pari_err(typeer,"substvec");
   if (lg(r)!=l)
     pari_err(talker,"different number of variables and values in substvec");
-  w=cgetg(l,t_VECSMALL);
-  z=cgetg(l,t_VECSMALL);
-  for(i=1;i<l;i++)
+  w = cgetg(l,t_VECSMALL);
+  z = cgetg(l,t_VECSMALL);
+  for(i=j=1;i<l;i++)
   {
     GEN T = gel(v,i);
     if (!gcmpX(T)) pari_err(talker,"not a variable in substvec (%Ps)", T);
-    w[i]=varn(T);
-    z[i]=fetch_var();
+    if (gvar(gel(r,i)) == NO_VARIABLE) /* no need to take precautions */
+      e = gsubst(e, varn(T), gel(r,i));
+    else
+    {
+      w[j]=varn(T);
+      z[j]=fetch_var();
+      j++;
+    }
   }
-  for(i=1;i<l;i++) e = gsubst(e,w[i],pol_x(z[i]));
-  for(i=1;i<l;i++) e = gsubst(e,z[i],gel(r,i));
-  for(i=1;i<l;i++) (void)delete_var();
+  for(i=1;i<j;i++) e = gsubst(e,w[i],pol_x(z[i]));
+  for(i=1;i<j;i++) e = gsubst(e,z[i],gel(r,i));
+  for(i=1;i<j;i++) (void)delete_var();
   return gerepileupto(ltop,e);
 }
 

src/basemath/polarit2.c
@@ -2344,7 +2344,7 @@ RgX_resultant_all(GEN P, GEN Q, GEN *sol)
   {
     delta = degpol(P) - degpol(Q); /* > 0 */
     Z = Lazard2(Q, leading_term(Q), s, delta);
-    if (!odd(delta)) sig = -sig;
+    if (both_odd(degpol(P), degpol(Q))) sig = -sig;
     Q = nextSousResultant(P, Q, Z, s);
     P = Z;
     if (low_stack(lim,stack_lim(av,1)))

src/test/32/resultant
@@ -400,4 +400,5 @@
 151666179549811431957
 1973
 [2, -2, 4]
-Total time spent: 8928
+y^4 + 2*y^3 + 3*y^2 + 2*y + 1
+Total time spent: 6068

src/test/in/resultant
@@ -58,3 +58,6 @@ for(i=1,15,if(rnfequation(polcyclo(17,'y),x+y^i,1)[2]^17!=1,print("error",i)))
 
 \\ #1233
 bezoutres(x^2+1, x^2-1)
+\\ #1234
+polresultant(x^4+x^2+1, x^4-y, x, 0)
+

commit 6e54e16a7996d681a870d9ba1ff711bde6b1cbba

fix substvec problem introduced in 961d38a24ef499 [#1306]
substvec(x+y,[x,y],[1,x]) -> 2

src/basemath/gen3.c
@@ -1487,27 +1487,29 @@ gsubstvec(GEN e, GEN v, GEN r)
 {
   pari_sp ltop=avma;
   long i, j, l = lg(v);
-  GEN w,z;
+  GEN w, z, R;
   if ( !is_vec_t(typ(v)) ) pari_err_TYPE("substvec",v);
   if ( !is_vec_t(typ(r)) ) pari_err_TYPE("substvec",r);
   if (lg(r)!=l) pari_err_DIM("substvec");
   w = cgetg(l,t_VECSMALL);
   z = cgetg(l,t_VECSMALL);
+  R = cgetg(l,t_VEC);
   for(i=j=1;i<l;i++)
   {
-    GEN T = gel(v,i);
+    GEN T = gel(v,i), ri = gel(r,i);
     if (!gcmpX(T)) pari_err(e_MISC,"not a variable in substvec (%Ps)", T);
-    if (gvar(gel(r,i)) == NO_VARIABLE) /* no need to take precautions */
-      e = gsubst(e, varn(T), gel(r,i));
+    if (gvar(ri) == NO_VARIABLE) /* no need to take precautions */
+      e = gsubst(e, varn(T), ri);
     else
     {
-      w[j]=varn(T);
-      z[j]=fetch_var();
+      w[j] = varn(T);
+      z[j] = fetch_var();
+      gel(R,j) = ri;
       j++;
     }
   }
   for(i=1;i<j;i++) e = gsubst(e,w[i],pol_x(z[i]));
-  for(i=1;i<j;i++) e = gsubst(e,z[i],gel(r,i));
+  for(i=1;i<j;i++) e = gsubst(e,z[i],gel(R,i));
   for(i=1;i<j;i++) (void)delete_var();
   return gerepileupto(ltop,e);
 }

src/test/in/subst
@@ -1,1 +1,2 @@
 subst(Y/X,X,x)
+substvec(x+y,[x,y],[1,x])