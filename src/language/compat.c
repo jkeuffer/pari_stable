jkeuffer/pari_stable

commit 3349d0f109637626e11780e874df4850f3a34c53

Support new operators in compat mode
_[_.._,_.._]
_[_.._]
_derivfun
_eval_mnemonic
_multi_if

src/language/compat.c
@@ -185,6 +185,8 @@ entree oldfonctions[]={
 {"_>>=_",0,(void*)gshiftre,13,"&L","_>>=_"},
 {"_>>_",0,(void*)gshift_right,13,"GL","x>>y"},
 {"_>_",0,(void*)ggt,13,"GG","_>_"},
+{"_[_.._,_.._]",0,(void*)matslice0,13,"GD0,L,D0,L,D0,L,D0,L,","x[a..b,c..d] = [x[a,c],  x[a+1,c],  ...,x[b,c];                      x[a,c+1],x[a+1,c+1],...,x[b,c+1];                        ...       ...          ...                      x[a,d],  x[a+1,d]  ,...,x[b,d]]"},
+{"_[_.._]",0,(void*)vecslice0,13,"GD0,L,L","x[a..b] = [x[a],x[a+1],...,x[b]]"},
 {"_\\/=_",0,(void*)gdivrounde,13,"&G","x\\\\/=y"},
 {"_\\/_",0,(void*)gdivround,13,"GG","x\\\\/y: rounded Euclidean quotient of x and y."},
 {"_\\=_",0,(void*)gdivente,13,"&G","x\\\\=y"},
@@ -192,6 +194,9 @@ entree oldfonctions[]={
 {"_^_",0,(void*)gpow,13,"GGp","_^_"},
 {"_^s",0,(void*)gpowgs,13,"GL","_^s"},
 {"__",0,NULL,13,NULL,"__"},
+{"_derivfun",0,(void*)derivfun0,15,"GGp","_derivfun(closure,[args]) numerical derivation of closure with respect to the first variable at (args)."},
+{"_eval_mnemonic",0,(void*)eval_mnemonic,15,"lGs","Convert a mnemonic string to a flag."},
+{"_multi_if",0,(void*)ifpari_multi,15,"GE*","internal variant of if() that allows more than 3 arguments."},
 {"_void_if",0,(void*)ifpari_void,11,"vGDIDI","internal variant of if() that does not return a value."},
 {"_||_",0,(void*)orpari,13,"GE","x||y"},
 {"_~",0,(void*)gtrans,13,"G","_~"},

src/test/64/compat
@@ -1853,7 +1853,7 @@ mod(x^2 - 3*x + 2, x^3 - 5*x^2 + 8*x - 5)
 [2, 2/3, 2/3, 2/3]
 ? nextprime(100000000000000000000000)
 100000000000000000000117
-? setrand(1);a=matrix(3,5,j,k,vvector(5,l,random()\10^8))
+? setrand(1);n=10^8;a=matrix(3,5,j,k,vvector(5,l,random()\n))
 
 [[15, 8, 17, 7, 12]~ [1, 4, 10, 6, 4]~ [1, 12, 9, 11, 11]~ [5, 0, 12, 21, 7]
 ~ [18, 12, 18, 3, 8]~]
@@ -1909,15 +1909,7 @@ mod(x^2 - 3*x + 2, x^3 - 5*x^2 + 8*x - 5)
 [-291920, 136855, 230560, -178520, 74190]~
 ? nfreduce(nf,ba,idx)
 [1, 0, 0, 0, 0]~
-? setrand(1);as=matrix(3,3,j,k,vvector(5,l,random()\10^8))
-
-[  [15, 8, 17, 7, 12]~   [1, 4, 10, 6, 4]~ [1, 12, 9, 11, 11]~]
-
-[[16, 15, 20, 18, 14]~  [0, 3, 21, 15, 8]~  [10, 0, 18, 0, 7]~]
-
-[  [11, 11, 12, 7, 2]~ [9, 20, 11, 7, 16]~  [0, 12, 3, 12, 2]~]
-
-? nfsmith(nf,[as,[1,1,1],[idealinv(nf,idx),idealinv(nf,idy),1]])
+? nfsmith(nf,[a[,1..3],[1,1,1],[idealinv(nf,idx),idealinv(nf,idy),1]])
 [[15706993357777254170417850, 1636878763571210697462070, 1307908830618593502
 9427775, 1815705333955314515809980, 7581330311082212790621785; 0, 5, 0, 0, 0
 ; 0, 0, 5, 0, 0; 0, 0, 0, 5, 0; 0, 0, 0, 0, 5], [1, 0, 0, 0, 0; 0, 1, 0, 0, 
@@ -2598,6 +2590,6 @@ mod(391, 3120), mod(2081, 3120)]]
 ? getstack()
 200
 ? getheap()
-[602, 96260]
+[600, 95850]
 ? print("Total time spent: ",gettime);
-Total time spent: 152
+Total time spent: 148

src/test/64/nfields
@@ -1,8 +1,7 @@
    echo = 1 (on)
 ? gettime;p2=Pol([1,3021,-786303,-6826636057,-546603588746,3853890514072057]);
 ? fa=[11699,6;2392997,2;4987333019653,2];
-? setrand(1);a=matrix(3,5,j,k,vectorv(5,l,random\10^8));
-? setrand(1);as=matrix(3,3,j,k,vectorv(5,l,random\10^8));
+? setrand(1);N=10^8;a=matrix(3,5,j,k,vectorv(5,l,random\N));
 ? nfpol=x^5-5*x^3+5*x+25;nf=nfinit(nfpol)
 [x^5 - 5*x^3 + 5*x + 25, [1, 2], 595125, 45, [[1, -1.08911514572050482502495
 27946671612684, -2.4285174907194186068992069565359418365, 0.7194669112891317
@@ -597,7 +596,7 @@ Mod(-1/2*x^5 + 9*x^2, x^6 + 108)
 [Mod(-2, y^3 - y - 1)]
 ? nfrootsof1(nf)
 [2, -1]
-? nfsnf(nf,[as,[1,1,1],[idealinv(nf,idx),idealinv(nf,idy),1]])
+? nfsnf(nf,[a[,1..3],[1,1,1],[idealinv(nf,idx),idealinv(nf,idy),1]])
 [[15706993357777254170417850, 1636878763571210697462070, 1307908830618593502
 9427775, 1815705333955314515809980, 7581330311082212790621785; 0, 5, 0, 0, 0
 ; 0, 0, 5, 0, 0; 0, 0, 0, 5, 0; 0, 0, 0, 0, 5], [1, 0, 0, 0, 0; 0, 1, 0, 0, 
@@ -734,6 +733,6 @@ x^15 - 15*x^11 + 75*x^7 - x^5 - 125*x^3 + 5*x + 1
 ? sizebyte(%)
 288
 ? getheap
-[178, 90098]
+[177, 89695]
 ? print("Total time spent: ",gettime);
-Total time spent: 80
+Total time spent: 88

src/test/in/compat
@@ -367,7 +367,7 @@ mu(3*5*7*11*13)
 \\ N
 newtonpoly(x^4+3*x^3+27*x^2+9*x+81,3)
 nextprime(100000000000000000000000)
-setrand(1);a=matrix(3,5,j,k,vvector(5,l,random()\10^8))
+setrand(1);N=10^8;a=matrix(3,5,j,k,vvector(5,l,random()\N))
 aid=[idx,idy,idz,idmat(5),idx]
 bb=algtobasis(nf,mod(x^3+x,nfpol))
 da=nfdetint(nf,[a,aid])
@@ -380,8 +380,7 @@ nfmod(nf,ba,bb)
 nfmul(nf,ba,bb)
 nfpow(nf,bb,5)
 nfreduce(nf,ba,idx)
-setrand(1);as=matrix(3,3,j,k,vvector(5,l,random()\10^8))
-nfsmith(nf,[as,[1,1,1],[idealinv(nf,idx), idealinv(nf,idy),1]])
+nfsmith(nf,[a[,1..3],[1,1,1],[idealinv(nf,idx), idealinv(nf,idy),1]])
 nfval(nf,ba,vp)
 norm(1+i)
 norm(mod(x+5,x^3+x+1))
@@ -619,5 +618,7 @@ zidealstar(nf2,54)
 bid=zidealstarinit(nf2,54)
 zideallog(nf2,w,bid)
 znstar(3120)
+sin'(4)
+m = 3; if(m == 1,2, m == 3,4,5)
 getstack()
 getheap()

src/test/in/nfields
@@ -4,8 +4,7 @@ dobnf(T,flag=0,tech=[])= setrand(1); my(K = bnfinit(T,flag,tech)); [K.cyc,K.fu];
 \\ Initialisations diverses
 gettime; p2=Pol([1,3021,-786303,-6826636057,-546603588746,3853890514072057]);
 fa=[11699,6;2392997,2;4987333019653,2];
-setrand(1);a=matrix(3,5,j,k,vectorv(5,l,random\10^8));
-setrand(1);as=matrix(3,3,j,k,vectorv(5,l,random\10^8));
+setrand(1);N=10^8;a=matrix(3,5,j,k,vectorv(5,l,random\N));
 nfpol=x^5-5*x^3+5*x+25; nf=nfinit(nfpol)
 nfinit(nfpol,2)
 nfinit(nfpol,3)[2]
@@ -118,7 +117,7 @@ nfisisom(x^3+x^2-2*x-1,x^3+x^2-2*x-1)
 nfisisom(x^3-2,nfinit(x^3-6*x^2-6*x-30))
 nfroots(nf2,x+2)
 nfrootsof1(nf)
-nfsnf(nf,[as, [1,1,1], [idealinv(nf,idx),idealinv(nf,idy),1]])
+nfsnf(nf,[a[,1..3], [1,1,1], [idealinv(nf,idx),idealinv(nf,idy),1]])
 nfsubfields(nf)
 polcompositum(x^4-4*x+2,x^3-x-1)
 polcompositum(x^4-4*x+2,x^3-x-1,1)

commit 0067ef5d5568e61f979a785c8d15b0993a98d42a

define "Str" when default(compatible,3)
+ fix whatnow(), broken by 3349d0f109

src/language/compat.c
@@ -202,6 +202,7 @@ entree oldfonctions[]={
 {"_~",0,(void*)gtrans,13,"G","_~"},
 {"O",0,(void*)ggrando,7,"GD1,L,","O(a^b): p-adic or power series zero with precision given by b."},
 {"O(_^_)",0,(void*)ggrando,7,"GD1,L,","O(a^b): p-adic or power series zero with precision given by b."},
+{"Str",0,(void*)Str,2,"s*","Str({x}*): concatenates its (string) argument into a single string."},
 {"abs",1,(void*)gabs,3,"Gp","abs(x)=absolute value (or modulus) of x."},
 {"acos",1,(void*)gacos,3,"Gp","acos(x)=inverse cosine of x."},
 {"acosh",1,(void*)gach,3,"Gp","acosh(x)=inverse hyperbolic cosine of x."},

src/whatnow
@@ -77,6 +77,8 @@ _>=_
 _>>=_
 _>>_
 _>_
+_[_.._,_.._]
+_[_.._]
 _\\/=_
 _\\/_
 _\\=_
@@ -84,9 +86,12 @@ _\\_
 _^_
 _^s
 __
+_derivfun
+_multi_if
 _void_if
 _||_
 _~
+Str
 abs
 acos
 acosh