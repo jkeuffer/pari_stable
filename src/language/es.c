jkeuffer/pari_stable

commit 0c68c334e38f760d3ca66580b77e6212982839da

faster _fputs_log [ avoid successive putc() ]; patch by Loic.

src/language/es.c
@@ -389,18 +389,20 @@ _fputs(const char *s, FILE *f ) {
 static void
 _putc_log(char c) { if (pari_logfile) (void)putc(c, pari_logfile); }
 static void
-_puts_log(const char *s) {
+_puts_log(const char *s)
+{
   FILE *f = pari_logfile;
-  const char *t;
+  char *p;
   if (!f) return;
-  if (logstyle == logstyle_color) { (void)fputs(s, f); return; }
-  for (t = s; *t; t++)
-  {
-    if (*t != esc) { (void)fputc(*t, f); continue; }
-    /* skip ANSI color escape sequence */
-    while (*++t != 'm')
-      if (!*t) return;
-  }
+  if (logstyle != logstyle_color)
+    while ( (p = strchr(s, esc)) )
+    { /* skip ANSI color escape sequence */
+      fwrite(s, 1, p-s-1, f);
+      s = strchr(p, 'm');
+      if (!s) return;
+      s++;
+    }
+  fputs(s, f);
 }
 static void
 _flush_log(void) { (void)fflush(pari_logfile); }

commit cb0ff9891b6825aaf6dbd078af9beb0fff59635b

_puts_log was discarding last character
Fix a bug introduced in rev 0c68c334e38

src/language/es.c
@@ -397,7 +397,7 @@ _puts_log(const char *s)
   if (logstyle != logstyle_color)
     while ( (p = strchr(s, esc)) )
     { /* skip ANSI color escape sequence */
-      fwrite(s, 1, p-s-1, f);
+      if ( p!=s ) fwrite(s, 1, p-s, f);
       s = strchr(p, 'm');
       if (!s) return;
       s++;