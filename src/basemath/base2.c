jkeuffer/pari_stable

commit 88c29b0455f438e14afc6a47508becfaa1073cc5

108- made public the CATCH / TRY interface, renamed pari_CATCH / pari_TRY       PARI functions err_get_num(), err_get_compo(), pari_err_last()

src/basemath/base2.c
@@ -576,10 +576,13 @@ nfmaxord(nfmaxord_t *S, GEN T, long flag, GEN fa)
     /* includes the silly case where P[i] = -1 */
     if (E[i] <= 1) { ordmax = shallowconcat(ordmax, gen_1); continue; }
     av = avma;
-    CATCH(e_INV) { /* caught false prime, update factorization */
-      GEN x = gel(global_err_data,3);
-      GEN N, p = gcdii(gel(x,1), gel(x,2)), u = diviiexact(gel(x,1),p);
+    pari_CATCH(e_INV) { /* caught false prime, update factorization */
+      GEN x, N, p, u, E = pari_err_last();
       long l;
+      x = err_get_compo(E, 2);
+      if (typ(x) != t_INTMOD) pari_err(0, E);
+      p = gcdii(gel(x,1), gel(x,2));
+      u = diviiexact(gel(x,1),p);
       if (DEBUGLEVEL) pari_warn(warner,"impossible inverse: %Ps", x);
       gerepileall(av, 2, &p, &u);
 
@@ -591,10 +594,10 @@ nfmaxord(nfmaxord_t *S, GEN T, long flag, GEN fa)
       av = avma;
       N = S->dT; E[i] = Z_pvalrem(N, gel(P,i), &N);
       for (k=lP, lP=lg(P); k < lP; k++) E[k] = Z_pvalrem(N, gel(P,k), &N);
-    } RETRY {
+    } pari_RETRY {
       if (DEBUGLEVEL) err_printf("Treating p^k = %Ps^%ld\n",P[i],E[i]);
       ordmax = shallowconcat(ordmax, mkvec( maxord(gel(P,i),T,E[i]) ));
-    } ENDCATCH;
+    } pari_ENDCATCH;
   }
   allbase_from_ordmax(S, ordmax, P, T);
 }

doc/usersch4.tex
@@ -1801,10 +1801,17 @@ and no remaining arguments. The general syntax is
 
 \noindent where \kbd{numerr} is a codeword which specifies the error class
 and what to do with the remaining arguments and what message to print.
-For instance, \kbd{pari\_err(e\_TYPE,"extgcd")} prints the message:
+For instance, if $x$ is a \kbd{GEN} with internal type \typ{STR}, say,
+\kbd{pari\_err(e\_TYPE,"extgcd", $x$)} prints the message:
 \bprog
-    ***   incorrect type in extgcd.
-@eprog\noindent See \secref{se:errors} for details.
+    ***   incorrect type in extgcd (t_STR),
+@eprog\noindent See \secref{se:errors} for details. In the libpari code
+itself, the general-purpose \kbd{e\_MISC} is used sparingly: it is so
+flexible that the corresponding error contexts (\typ{ERROR}) become hard to
+use reliably. Other more rigid error types are generally more useful: for
+instance the error context associated to the \kbd{e\_TYPE} exception above is
+precisely documented and contains \kbd{"extgcd"} and $x$ (not only its type)
+as readily available components.
 
 \subsec{Warnings}
 
@@ -2115,6 +2122,82 @@ Since you can set \kbd{E} to a pointer to any \kbd{struct} (typecast to
 \kbd{void*}) the above mechanism handles arbitrary functions. For simple
 functions without extra parameters, you may set \kbd{E = NULL} and ignore
 that argument in your function definition.
+
+\section{Catching exceptions}
+
+\subsec{Basic use}
+
+PARI provides a mechanism to trap exceptions generated via \kbd{pari\_err}
+using the \tet{pari_CATCH} construction. The basic usage is as follows
+\bprog
+ pari_CATCH(err_code) {
+   @com recovery branch
+ }
+ pari_TRY {
+   @com main branch
+ }
+ pari_ENDCATCH
+@eprog\noindent This fragment executes the main branch, then the recovery
+branch \emph{if} exception \kbd{err\_code} is thrown, e.g. \kbd{e\_TYPE}.
+See \secref{se:errors} for the description of all error classes.
+The special error code \tet{CATCH_ALL} is available to catch all errors.
+
+One can replace the \tet{pari_TRY} keyword by \tet{pari_RETRY}, in which case
+once the recovery branch is run, we run the main branch again, still catching
+the same exceptions.
+
+\misctitle{Restrictions}
+
+\item Such constructs can be nested without adverse effect, the innermost
+handler catching the exception.
+
+\item It is \emph{valid} to leave either branch using \tet{pari_err}.
+
+\item It is \emph{invalid} to use C flow control instructions (\kbd{break},
+\kbd{continue}, \kbd{return}) to directly leave either branch without seeing
+the \tet{pari_ENDCATCH} keyword. This would leave an invalid structure in the
+exception handler stack, and the next exception would crash.
+
+\item In order to leave using \kbd{break}, \kbd{continue} or \kbd{return},
+one must precede the keyword by a call to
+
+\fun{void}{pari_CATCH_reset}{} disable the current handler, allowing to leave
+without adverse effect.
+
+\subsec{Advanced use}
+
+In the recovery branch, the exception context can be examined via the
+following helper routines:
+
+\fun{GEN}{pari_err_last}{} returns the exception context, as a \typ{ERROR}.
+The exception $E$ returned by \tet{pari_err_last} can be rethrown, using
+\bprog
+  pari_err(0, E);
+@eprog
+
+\fun{long}{err_get_num}{GEN E} returns the error symbolic name. E.g
+\kbd{e\_TYPE}.
+
+\fun{GEN}{err_get_compo}{GEN E, long i} error $i$-th component, as documented
+in \secref{se:iferrname}.
+
+\noindent For instance
+\bprog
+ pari_CATCH(CATCH_ALL) { /* catch everything */
+    GEN x, E = pari_err_last();
+    long code = err_get_num(E);
+    if (code != e_INV) pari_err(0, E); /* unexpected error, rethrow */
+    x = err_get_compo(E, 2);
+    /* e_INV has two components, 1: function name 2: non-invertible x */
+    if (typ(x) != t_INTMOD) pari_err(0, E); /* unexpected type, rethrow */
+    pari_CATCH_reset();
+    return x; /* leave ! */
+    @com @dots
+ } pari_TRY {
+   @com main branch
+ }
+ pari_ENDCATCH
+@eprog
 
 \section{A complete program}
 \label{se:prog}

src/basemath/FpV.c
@@ -938,13 +938,16 @@ GEN
 FpMs_FpCs_solve_safe(GEN M, GEN A, long nbrow, GEN p)
 {
   GEN res;
-  CATCH(e_INV)
+  pari_CATCH(e_INV)
   {
+    GEN E = pari_err_last();
+    GEN x = err_get_compo(E,2);
+    if (typ(x) != t_INTMOD) pari_err(0,E);
     if (DEBUGLEVEL)
-      pari_warn(warner,"FpMs_FpCs_solve_safe, impossible inverse %Ps", gel(global_err_data,3));
+      pari_warn(warner,"FpMs_FpCs_solve_safe, impossible inverse %Ps", x);
     res = NULL;
-  } TRY {
+  } pari_TRY {
     res = ZpMs_ZpCs_solve(M, A, nbrow, p, 1);
-  } ENDCATCH
+  } pari_ENDCATCH
   return res;
 }

src/basemath/bibli1.c
@@ -1611,14 +1611,14 @@ fincke_pohst(GEN a, GEN B0, long stockmax, long PREC, FP_chk_fun *CHECK)
   for (i=1; i<l; i++) { uperm[l-i] = u[perm[i]]; rperm[l-i] = r[perm[i]]; }
   u = uperm;
   r = rperm; res = NULL;
-  CATCH(e_PREC) { }
-  TRY {
+  pari_CATCH(e_PREC) { }
+  pari_TRY {
     GEN q;
     if (CHECK && CHECK->f_init) bound = CHECK->f_init(CHECK, r, u);
     q = Q_from_QR(r, gprecision(vnorm));
     if (!q) pari_err_PREC("fincke_pohst");
     res = smallvectors(q, bound, stockmax, CHECK);
-  } ENDCATCH;
+  } pari_ENDCATCH;
   if (CHECK)
   {
     if (CHECK->f_post) res = CHECK->f_post(CHECK, res, u);

src/basemath/buch2.c
@@ -2884,8 +2884,8 @@ static GEN
 bestappr_noer(GEN x, GEN k)
 {
   GEN y;
-  CATCH(e_PREC) { y = NULL; }
-  TRY { y = bestappr(x,k); } ENDCATCH;
+  pari_CATCH(e_PREC) { y = NULL; }
+  pari_TRY { y = bestappr(x,k); } pari_ENDCATCH;
   return y;
 }
 

src/basemath/gen2.c
@@ -956,16 +956,17 @@ static int
 gequal_try(GEN x, GEN y)
 {
   int i;
-  CATCH(CATCH_ALL) {
-    switch(err_get_num(global_err_data))
+  pari_CATCH(CATCH_ALL) {
+    GEN E = pari_err_last();
+    switch(err_get_num(E))
     {
       case e_STACK: case e_MEM: case e_ALARM:
-        pari_err(0, global_err_data);
+        pari_err(0, E); /* rethrow */
     }
     return 0;
-  } TRY {
+  } pari_TRY {
     i = gequal0(gadd(x, gneg_i(y)));
-  } ENDCATCH;
+  } pari_ENDCATCH;
   return i;
 }
 

src/functions/programming/iferrname
@@ -146,8 +146,10 @@ Doc: evaluates the expression sequence \var{seq1}. If an error of type
   \var{E} has three component, 1 (\typ{STR}): the function name $s$,
   2: the argument $x$, 3: the argument $y$.
 
-  \item \kbd{"e\_INV"}. Tried to invert a non-invertible object $x$.
-  \var{E} has one component, the non-invertible $x$. If $x = \kbd{Mod}(a,b)$
+  \item \kbd{"e\_INV"}. Tried to invert a non-invertible object $x$ in
+  function $s$.
+  \var{E} has two components, 1 (\typ{STR}): the function name $s$,
+  2: the non-invertible $x$. If $x = \kbd{Mod}(a,b)$
   is a \typ{INTMOD} and $a$ is not $0$ mod $b$, this allows to factor
   the modulus, as \kbd{gcd}$(a,b)$ is a non-trivial divisor of $b$.
 

src/gp/gp.c
@@ -1932,15 +1932,13 @@ closure_alarmer(GEN C, long s)
 #ifndef HAS_ALARM
   pari_err(e_ARCH,"alarm");
 #endif
-  CATCH(CATCH_ALL) /* We need to stop the timer after any error */
+  pari_CATCH(CATCH_ALL) /* We need to stop the timer after any error */
   {
-    if (err_get_num(global_err_data)!=e_ALARM)
-    {
-      alarm0(0); pari_err(0, global_err_data);
-    }
-    evalstate_restore(&state); x = gerepilecopy(avma, global_err_data);
+    GEN E = pari_err_last();
+    if (err_get_num(E) != e_ALARM) { alarm0(0); pari_err(0, E); }
+    evalstate_restore(&state); x = gerepilecopy(avma, E);
   }
-  TRY { alarm0(s); x = closure_evalgen(C); alarm0(0); } ENDCATCH;
+  pari_TRY { alarm0(s); x = closure_evalgen(C); alarm0(0); } pari_ENDCATCH;
   return x;
 }
 

commit 561449326b52ac2737da3bb00a8cc1c94afc5122

Typo: variable E shadowed [#1365]
introduced in 88c29b04

src/basemath/base2.c
@@ -577,10 +577,10 @@ nfmaxord(nfmaxord_t *S, GEN T, long flag, GEN fa)
     if (E[i] <= 1) { ordmax = shallowconcat(ordmax, gen_1); continue; }
     av = avma;
     pari_CATCH(e_INV) { /* caught false prime, update factorization */
-      GEN x, N, p, u, E = pari_err_last();
+      GEN x, N, p, u, ERR = pari_err_last();
       long l;
-      x = err_get_compo(E, 2);
-      if (typ(x) != t_INTMOD) pari_err(0, E);
+      x = err_get_compo(ERR, 2);
+      if (typ(x) != t_INTMOD) pari_err(0, ERR);
       p = gcdii(gel(x,1), gel(x,2));
       u = diviiexact(gel(x,1),p);
       if (DEBUGLEVEL) pari_warn(warner,"impossible inverse: %Ps", x);