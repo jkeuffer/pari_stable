jkeuffer/pari_stable

commit 41f79ff2a5ca6adea208d9dd14174856b57ae281

remove dependency on readline.h in gp.c

src/gp/gp.c
@@ -30,12 +30,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 #  endif
 #endif
 
-#ifdef READLINE
-BEGINEXTERN
-#  include <readline/readline.h>
-ENDEXTERN
-#endif
-
 /********************************************************************/
 /**                                                                **/
 /**                            STRINGS                             **/
@@ -780,34 +774,14 @@ aide(const char *s, long flag)
 static char *
 what_readline(void)
 {
-  char *s;
 #ifdef READLINE
-  const char *ver;
-  char *extra = stack_malloc(strlen(READLINE) + 32);
-#  if defined(HAS_RL_LIBRARY_VERSION) || defined(FAKE_RL_LIBRARY_VERSION)
-#    ifdef FAKE_RL_LIBRARY_VERSION
-  extern char *rl_library_version;
-#    endif
-
-  if (strcmp(READLINE, rl_library_version))
-  {
-    ver = (char*)rl_library_version;
-    (void)sprintf(extra, " [was v%s in Configure]", READLINE);
-  }
-  else
-#  endif
-  {
-    ver = READLINE;
-    extra[0] = 0;
-  }
-  s = stack_malloc(3 + strlen(ver) + 8 + strlen(extra));
-  (void)sprintf(s, "v%s %s%s", ver,
-            (GP_DATA->use_readline)? "enabled": "disabled",
-            extra);
-#else
-  s = (char*)"not compiled in";
-#endif
+  const char *v = READLINE;
+  char *s = stack_malloc(3 + strlen(v) + 8);
+  (void)sprintf(s, "v%s %s", v, GP_DATA->use_readline? "enabled": "disabled");
   return s;
+#else
+  return (char*)"not compiled in";
+#endif
 }
 
 static void

config/Makefile.SH
@@ -678,7 +678,7 @@ for dir in basemath modules language gp graph systems; do
   compile='$(CC)'
   case "$f" in
   gp)
-    cflags="$cflags -I$src/language \$(RLINCLUDE)"
+    cflags="$cflags -I$src/language"
     depend="$src/gp/gp.h"
     ;;
   gp_rl)

commit bd17db30a7d54931da647c67a0620aaabfc8cf31

Fix problem introduced in  41f79ff2
RL_PROMPT_START_IGNORE is no longer defined in gp.c: move relevant code
to gp_rl.c

src/gp/gp.c
@@ -1355,14 +1355,10 @@ static void
 brace_color(char *s, int c, int force)
 {
   if (disable_color || (gp_colors[c] == c_NONE && !force)) return;
-#ifdef RL_PROMPT_START_IGNORE
-  if (GP_DATA->use_readline) {
-    *s++ = RL_PROMPT_START_IGNORE;
-    term_get_color(s, c);
-    s += strlen(s);
-    *s++ = RL_PROMPT_END_IGNORE;
-    *s = 0;
-  } else
+#ifdef READLINE
+  if (GP_DATA->use_readline)
+    readline_prompt_color(s, c);
+  else
 #endif
     term_get_color(s, c);
 }

src/gp/gp.h
@@ -23,6 +23,7 @@ BEGINEXTERN
 void aide(const char *s, long flag);
 void echo_and_log(const char *prompt, const char *s);
 int  get_line_from_readline(const char *prompt, const char *prompt_cont, filtre_t *F);
+void readline_prompt_color(char *s, int c);
 void gp_output(GEN z, gp_data *G);
 void init_readline(void);
 void texmacs_completion(const char *s, long pos);

src/gp/gp_rl.c
@@ -679,6 +679,20 @@ init_readline(void)
   rl_bind_key_in_map(2, pari_rl_backward_sexp, emacs_meta_keymap); /* M-C-b */
 }
 
+void
+readline_prompt_color(char *s, int c)
+{
+#ifdef RL_PROMPT_START_IGNORE
+  *s++ = RL_PROMPT_START_IGNORE;
+  term_get_color(s, c);
+  s += strlen(s);
+  *s++ = RL_PROMPT_END_IGNORE;
+  *s = 0;
+#else
+  term_get_color(s, c);
+#endif
+}
+
 /* readline-specific defaults */
 GEN
 sd_readline(const char *v, long flag)