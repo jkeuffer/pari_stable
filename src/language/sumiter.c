jkeuffer/pari_stable

commit 4f66f07d97f0446e14744ec76ef44c3a8904ca5a

7- [libpari] forcomposite_init / forcomposite_next, reimplement    forcomposite() using this iterator
+ add 'iterator' bench
+ add missing forprime_init / forprime_next doc

src/language/sumiter.c
@@ -523,42 +523,60 @@ forprime(GEN a, GEN b, GEN code)
   pop_lex(1); avma = av;
 }
 
+int
+forcomposite_init(forcomposite_t *C, GEN a, GEN b)
+{
+  pari_sp av = avma;
+  a = gceil(a); if (typ(a)!=t_INT) pari_err_TYPE("forcomposite",a);
+  if (b) {
+    b = gfloor(b);if (typ(b)!=t_INT) pari_err_TYPE("forcomposite",b);
+  }
+  if (signe(a) < 0) pari_err_DOMAIN("forcomposite", "a", "<", gen_0, a);
+  if (cmpiu(a, 4) < 0) a = utoipos(4);
+  if (!forprime_init(&C->T, a,b)) { avma = av; return 0; }
+  C->first = 1;
+  C->n = setloop(a);
+  C->b = b;
+  C->p = NULL; return 1;
+}
+
+GEN
+forcomposite_next(forcomposite_t *C)
+{
+  if (C->first) /* first call ever */
+  {
+    C->first = 0;
+    C->p = forprime_next(&C->T);
+    if (C->p && equalii(C->n, C->p)) incloop(C->n);
+    return C->n;
+  }
+  incloop(C->n);
+  if (C->p)
+  {
+    if (cmpii(C->n, C->p) < 0) return C->n;
+    incloop(C->n);
+    /* n = p+1 */
+    C->p = forprime_next(&C->T); /* nextprime(p) > n */
+    if (C->p) return C->n;
+  }
+  if (!C->b || cmpii(C->n, C->b) <= 0) return C->n;
+  return NULL;
+}
+
 void
 forcomposite(GEN a, GEN b, GEN code)
 {
   pari_sp av = avma;
-  GEN n, p;
-  forprime_t T;
-
-  a = gceil(a);
-  b = gfloor(b);
-  if (!forprime_init(&T, a,b)) { avma = av; return; }
-  if (signe(a) < 0) pari_err_DOMAIN("forcomposite", "a", "<", gen_0, a);
-  if (cmpiu(a, 2) < 0) a = gen_2;
-
-  n = setloop(a);
-
-  push_lex(n,code);
-  while( (p = forprime_next(&T)))
-  {
-    while(cmpii(n, p) < 0)
-    {
-      closure_evalvoid(code); if (loop_break()) break;
-      /* n changed in 'code', complain */
-      if (get_lex(-1) != n)
-        pari_err(e_MISC,"index read-only: was changed to %Ps", get_lex(-1));
-      incloop(n);
-    }
-    incloop(n);
-    /* n = p+1 */
-  }
-  while(cmpii(n, b) <= 0)
+  forcomposite_t T;
+  GEN n;
+  if (!forcomposite_init(&T,a,b)) return;
+  push_lex(T.n,code);
+  while((n = forcomposite_next(&T)))
   {
     closure_evalvoid(code); if (loop_break()) break;
     /* n changed in 'code', complain */
     if (get_lex(-1) != n)
       pari_err(e_MISC,"index read-only: was changed to %Ps", get_lex(-1));
-    incloop(n);
   }
   pop_lex(1); avma = av;
 }

src/test/32/iterator
@@ -1,0 +1,21 @@
+4
+6
+8
+9
+10
+6
+8
+10
+6
+8
+9
+10
+12
+6
+8
+9
+6
+  ***   at top-level: forcomposite(a=6,12,print(a);a=1)
+  ***                                     ^-------------
+  ***   index read-only: was changed to 1.
+Total time spent: 0

src/test/in/iterator
@@ -1,0 +1,5 @@
+forcomposite(a=2,10,print(a))
+forcomposite(a=5,11,print(a))
+forcomposite(a=6,12,print(a))
+forcomposite(a=6,,print(a); if (a>8, break))
+forcomposite(a=6,12,print(a); a=1)

doc/usersch5.tex
@@ -3053,13 +3053,18 @@ would be exhibited in this way, though.) This should be slower than
 \tet{BPSW_psp} for $k\geq 4$ and we would expect it to be less reliable.
 
 \subsec{Iterators over primes}
+
+\fun{int}{forprime_init}{forprime_t *T, GEN a, GEN b} initialize an
+iterator $T$ over primes in $[a,b]$; over primes $\geq a$ if $b =
+\kbd{NULL}$. Return $0$ if the range is known to be empty from the start
+(as if $b < a$ or $b < 0$), and return $1$ otherwise.
+
+\fun{GEN}{forprime_next}{forprime_t *T} returns the next prime in the range,
+assuming that $T$ was initialized by \tet{forprime_init}.
+
 \fun{int}{u_forprime_init}{forprime_t *T, ulong a, ulong b}
 
-\fun{int}{forprime_init}{forprime_t *T, GEN a, GEN b}
-
 \fun{ulong}{u_forprime_next}{forprime_t *T}
-
-\fun{GEN}{forprime_next}{forprime_t *T}
 
 \fun{void}{u_forprime_restrict}{forprime_t *T, ulong c} let $T$ an iterator
 over primes initialized via \kbd{u\_forprime\_init(\&T, a, b)}, possibly
@@ -3069,6 +3074,16 @@ b$. Restrict the range of primes considered to $[a,c]$.
 \fun{int}{u_forprime_arith_init}{forprime_t *T, ulong a,ulong b, ulong c,ulong q} initialize an iterator over primes in $[a,b]$, congruent to $c$
 modulo $q$. Assume $0 \leq c < q$ and $(c,q) = 1$. Subsequent calls to
 \tet{u_forprime_next} will only return primes congruent to $c$ modulo $q$.
+
+\subsec{Iterators over composite integers}
+
+\fun{int}{forcomposite}{forcomposite_t *T, GEN a, GEN b} initialize an
+iterator $T$ over composite integers in $[a,b]$; over composites $\geq a$ if
+$b = \kbd{NULL}$. Return $0$ if the range is known to be empty from the start
+(as if $b < a$ or $b < 0$), and return $1$ otherwise.
+
+\fun{GEN}{forcomposite_next}{forcomposite_t *T} returns the next composite in
+the range, assuming that $T$ was initialized by \tet{forcomposite_init}.
 
 \subsec{Pseudo-random integers}
 These routine return pseudo-random integers uniformly distributed in some

src/headers/paristio.h
@@ -47,6 +47,12 @@ typedef struct {
   GEN pp;
 } forprime_t;
 
+typedef struct {
+  int first;
+  GEN b, n, p;
+  forprime_t T;
+} forcomposite_t;
+
 /* Iterate over partitions */
 typedef struct
 {

commit 68cbc5ab513b11eceefefc49bd92d3b886109df5

forcomposite: fix bug introduced in  4f66f07 [#1431]
Apply patch contributed by Esa Peuha

src/language/sumiter.c
@@ -547,10 +547,9 @@ forcomposite_next(forcomposite_t *C)
   {
     C->first = 0;
     C->p = forprime_next(&C->T);
-    if (C->p && equalii(C->n, C->p)) incloop(C->n);
-    return C->n;
   }
-  incloop(C->n);
+  else
+    incloop(C->n);
   if (C->p)
   {
     if (cmpii(C->n, C->p) < 0) return C->n;

src/test/32/iterator
@@ -5,6 +5,7 @@
 10
 6
 8
+9
 10
 6
 8