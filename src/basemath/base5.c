75290ef11d19ee0958623d2066d10e9d6f190fa3
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2014-01-15 15:44:31 +0100

    fix rnfeltabstorel when base field is Q and rnfequation's k is 0 [#1512]

--- src/basemath/base5.c
+++ src/basemath/base5.c
@@ -58,7 +58,15 @@ rnfeltreltoabs(GEN rnf,GEN x)
     case t_INT: return icopy(x);
     case t_FRAC: return gcopy(x);
     case t_POLMOD:
-      if (RgX_equal_var(gel(x,1), rnf_get_polabs(rnf))) return gcopy(x);
+      if (RgX_equal_var(gel(x,1), pol))
+      { /* already in 'abs' form, unless possibly if nf = Q */
+        if (typ(gel(x,2)) == t_POLMOD)
+        {
+          x = gel(x,2);
+          retmkpolmod(gcopy(gel(x,2)), RgX_copy(pol));
+        }
+        return gcopy(x);
+      }
       x = polmod_nffix(f,rnf,x,0);
       if (typ(x) == t_POLMOD) return rnfeltup(rnf,x);
       retmkpolmod(eltreltoabs(rnf_get_map(rnf), x), RgX_copy(pol));
