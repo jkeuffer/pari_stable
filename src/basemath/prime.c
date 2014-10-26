52449d2e4871b39370ef8983e29ed46fda12eeb3
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2014-10-26 13:33:38 +0100

    65- primes([2^50, 2^50+200000]) => stack overflow

--- src/basemath/prime.c
+++ src/basemath/prime.c
@@ -1230,7 +1230,11 @@ primes_interval_zv(ulong a, ulong b)
   if (!a) return primes_upto_zv(b);
   if (b < a) return cgetg(1, t_VECSMALL);
   d = b - a;
-  if (d > 100000UL) d = primepi_upper_bound(b) - primepi_lower_bound(a);
+  if (d > 100000UL)
+  {
+    ulong D = (ulong)ceil(primepi_upper_bound(b)-primepi_lower_bound(a));
+    if (D < d) d = D;
+  }
   return primes_interval_i(a, b, d);
 }
 GEN
