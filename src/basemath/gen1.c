45d54180084548802c5130adf127bfa8eecb65cd
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2015-01-11 15:48:35 +0100

    div_scal_rfrac could create an invalid t_POL [#1651]
    if t_FRAC has a constant numerator, function returns a t_POL whose
    coeffs are t_POLs in the main variable

--- src/basemath/gen1.c
+++ src/basemath/gen1.c
@@ -2322,8 +2322,11 @@ div_scal_rfrac(GEN x, GEN y)
 {
   GEN y1 = gel(y,1), y2 = gel(y,2);
   pari_sp av = avma;
-  if (typ(y1) == t_POL && varn(y2) == varn(y1) && degpol(y1) > 0)
-    return gerepileupto(av, gred_rfrac_simple(gmul(x, y2), y1));
+  if (typ(y1) == t_POL && varn(y2) == varn(y1))
+  {
+    if (degpol(y1)) return gerepileupto(av, gred_rfrac_simple(gmul(x, y2), y1));
+    y1 = gel(y1,2);
+  }
   return RgX_Rg_mul(y2, gdiv(x,y1));
 }
 static GEN
